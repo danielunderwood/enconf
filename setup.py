5328c34aa4c08f436a6befc9d5e8bac547207a29
Daniel Underwood <daniel.underwood13@gmail.com>
2018-09-08 19:29:03 -0400

    Use separate version file

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
+from distutils.util import convert_path
 
-import .enconf
+def get_version():
+    filepath = convert_path('enconf/version.py')
+    namespace = {}
+    with open(filepath) as version_file:
+        exec(version_file, namespace)
+
+    return namespace['__version__']
 
 setup(
     name='enconf',
-    version=enconf.__version__,
+    version=get_version(),
     description='Easy config setup from the environment',
     url='https://github.com/danielunderwood/enconf',
     author='Daniel Underwood',
